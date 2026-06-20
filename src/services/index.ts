e531e5e625295619f212bdfd822e4b87ac7357fd
Renoo AI <renoo@menuxpro.com>
2026-06-20 03:55:39 +0100

    Cleanup: delete all Firebase services, contexts, security bloat, prisma, functions, junk

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { restaurantService } from './restaurantService';
-export { menuService } from './menuService';
-export { tableService } from './tableService';
-export { orderService } from './orderService';
-export { authService } from './authService';
-export { logService } from './logService';
